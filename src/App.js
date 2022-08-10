0b2044a578bd34f9005368c403f863533ef6a6eb
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-08-10 20:02:53 +0300

    exchange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
+import { XMLParser } from 'fast-xml-parser';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [result, setResult] = useState([])
+  const currencyLisn = result.map()(e => {
+    return <tr> <td>{e.Name}</td><td>{e.Value}</td> </tr>
+  })
+
+  function sortName() {
+    const result2 = [...result]
+    result2.sort((a, b) => a.Name.localCompare(b.Name))
+    setResult(result2)
+  }
+
+  function push() {
+    fetch("https://www.cbr-xml-daily.ru/daily_utf8.xml")
+      .then(response => response.text())
+      .then(parser => new XMLParser().parser(parser))
+      .then(result => setResult(result.ValCurs.Valute))
+  }
+  return (
+    <div>
+      <button className='test' onClick={push}> UP </button>
+      <button className='filter' onClick={sortName}></button>
+      <table>
+        <tr>
+          <td></td>
+          <td></td>
+        </tr>
+        {currencyLisn}
+      </table>
+    </div>
+  )
+}
 export default App;
+
+
