fea5ed563c1130bc7589df7c9d05825bd082eba0
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-08-12 00:49:42 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,38 +3,41 @@ import './App.css';
 import React, { useState } from 'react';
 import { XMLParser } from 'fast-xml-parser';
 
-function App() {
 
+function App() {
+  
   const [result, setResult] = useState([])
-  const currencyLisn = result.map()(e => {
-    return <tr> <td>{e.Name}</td><td>{e.Value}</td> </tr>
+  const currencyList = result.map(e => {
+    return <tr><td>{e.Name}</td><td>{e.Name}</td></tr>
   })
 
   function sortName() {
     const result2 = [...result]
-    result2.sort((a, b) => a.Name.localCompare(b.Name))
+    result2.sort((a, b) => a.Name.localCompare((b.Name))
     setResult(result2)
   }
 
   function push() {
-    fetch("https://www.cbr-xml-daily.ru/daily_utf8.xml")
+    fetch('https://www.cbr-xml-daily.ru/daily_utf8.xml')
       .then(response => response.text())
-      .then(parser => new XMLParser().parser(parser))
+      .then(parser => new XMLParser().parse(parser))
       .then(result => setResult(result.ValCurs.Valute))
   }
   return (
     <div>
       <button className='test' onClick={push}> UP </button>
-      <button className='filter' onClick={sortName}></button>
+      <button className='test' onClick={sortName}>  </button>
       <table>
         <tr>
           <td></td>
           <td></td>
         </tr>
-        {currencyLisn}
+        {currencyList}
       </table>
     </div>
   )
+
+
 }
 export default App;
 
